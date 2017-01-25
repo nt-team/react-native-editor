7d2788d3fd5323d6627fabd31d333af6d1e26515
huangqi03 <huangqi03@baidu.com>
2017-01-25 22:03:05 +0800

    add props

--- src/component.tsx
+++ src/component.tsx
@@ -5,16 +5,22 @@ import {
 import createInvoke, { IMessager } from 'react-native-webview-invoke/native'
 
 export interface RNEditorProperties {
-
+    source?: number | React.WebViewUriSource | React.WebViewHtmlSource
+    autoHeight?: boolean
 }
 
 export default class RNEditor extends React.Component<RNEditorProperties, any>{
+    static defaultProps = {
+        source: require('../lib/web/dist/RNEditor.html'),
+        autoHeight: false
+    }
     private webview: WebView
     private invoke: IMessager = createInvoke(() => this.webview)
     render () {
         return <WebView
             ref={(w: any) => this.webview = w}
-            source={require('../lib/web/dist/RNEditor.html')}
+            source={this.props.source}
+            bounces={!this.props.autoHeight}
             onMessage={this.invoke.listener}
         />
     }
