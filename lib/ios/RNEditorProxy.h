4e6ce4482106818cff43e3e7db84356e6b41a22b
huangqi03 <huangqi03@baidu.com>
2017-02-13 20:04:27 +0800

    init proxy

---
+++ lib/ios/RNEditorProxy.h
@@ -0,0 +1,13 @@
+//
+//  RNEditorProxy.h
+//  RNEditorWebView
+//
+//  Created by 黄祺 on 2017/2/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNEditorProxy : NSObject
+
+@end
---
+++ lib/ios/RNEditorProxy.m
@@ -0,0 +1,13 @@
+//
+//  RNEditorProxy.m
+//  RNEditorWebView
+//
+//  Created by 黄祺 on 2017/2/13.
+//
+//
+
+#import "RNEditorProxy.h"
+
+@implementation RNEditorProxy
+
+@end
