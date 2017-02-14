0406edde1536f8bb5e83f41ae9bad8af1a7174d9
huangqi03 <huangqi03@baidu.com>
2017-02-14 18:35:53 +0800

    push

--- lib/ios/RNEditorProxy.m
+++ lib/ios/RNEditorProxy.m
@@ -8,6 +8,49 @@
 
 #import "RNEditorProxy.h"
 
+
+@interface RNEditorProxyProtocol : NSURLProtocol
+
+@end
+
+@implementation RNEditorProxyProtocol {
+    NSMutableURLRequest* _correctedRequest;
+}
+
++ (BOOL)canInitWithRequest:(NSURLRequest *)request {
+    return YES;
+}
+
++ (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request {
+    return request;
+}
+
++ (BOOL)requestIsCacheEquivalent:(NSURLRequest *)a toRequest:(NSURLRequest *)b {
+    return NO;
+}
+
+- (instancetype)initWithRequest:(NSURLRequest *)request cachedResponse:(NSCachedURLResponse *)cachedResponse client:(id<NSURLProtocolClient>)client {
+    if (self = [super initWithRequest:request cachedResponse:cachedResponse client:client]) {
+        _correctedRequest = request.mutableCopy;
+    }
+    return self;
+}
+
+- (void)startLoading {
+    
+}
+
+- (void)stopLoading {
+    _correctedRequest = nil;
+}
+
+@end
+
+
 @implementation RNEditorProxy
+
++ (void)initialize {
+    [NSURLProtocol registerClass:[RNEditorProxyProtocol class]];
+}
 
 @end
