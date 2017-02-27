f37dac3ebb890533d395cc2eba0e362dade56e38
huangqi03 <huangqi03@baidu.com>
2017-02-27 16:49:39 +0800

    initial android module

---
+++ android/app/src/main/java/com/rneditor/webview/RNEditorWebViewManager.java
@@ -0,0 +1,10 @@
+package com.rneditor.webview;
+
+import com.facebook.react.views.webview.ReactWebViewManager;
+
+/**
+ * Created by huangqi on 2017/2/27.
+ */
+
+public class RNEditorWebViewManager extends ReactWebViewManager {
+}
